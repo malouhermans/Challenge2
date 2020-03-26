a7e6c00ab6ad51da4eb3c6b22fb541ddf9648fea
malouhermans <61313164+malouhermans@users.noreply.github.com>
2020-03-26 17:47:18 +0100

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,55 @@
+function show(){
+	// maakt tijdsfunctie aan
+	var today = new Date();
+	// zorgt dat de huidige uren, minuten en seconden gerbuikt worden
+	var hours = today.getHours();
+    var minutes = today.getMinutes();
+    var seconds = today.getSeconds();
+    // plaatst een nul voor een enkel getal
+	    if (hours <= 9){
+	        hours = '0' + minutes;
+	    }
+	    if (minutes <= 9){
+	        minutes = '0' + minutes;
+	    }
+	    if (seconds <= 9){
+	        seconds = '0' + seconds;
+	    }
+
+    var date = today.getDate();
+    /* zorgt ervoor dat de maanden niet beginnen bij key 0,
+   	maar bij 1, zoals de normale kalender */
+    var month = (today.getMonth()+1);
+    var year = today.getFullYear();
+    document.getElementById('clock').innerHTML = hours+":"+minutes+":"+seconds;
+    document.getElementById('date').innerHTML = date+"-"+month+"-"+year;
+    
+    setTimeout("show()",1000);
+
+}
+
+function showImage(image){
+	var img = document.createElement("IMG");
+    img.src = image;
+	document.getElementById('image').appendChild(img);
+}
+
+// Zorgt voor het veranderen van het maan en zon icoontje
+function dayNight(){
+	var today1 = new Date();
+	var hours1 = today1.getHours(); 
+	if (hours1 >= 8 && hours1 <= 24){ 
+		showImage(image = "fotos/sun.png");
+	}
+    else{
+    	showImage(image = "fotos/moon.png");
+	}
+}
+
+// Het aanroepen van de functies, wanneer de pagina geladen wordt
+window.onload = function(){
+	show();
+	dayNight();
+};
+
+
